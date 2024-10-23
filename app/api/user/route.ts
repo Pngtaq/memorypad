d531c5635e6455ebfe4583f314aac07ed151e8ec
RaisonSalvador <raisonsalvador0@gmail.com>
2024-10-23 21:21:46 +0800

    checking

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -36,10 +36,3 @@ export async function POST(req: Request) {
     }
   }
 }
-
-// Optionally, you can add a method handler for unsupported methods
-// export async function METHOD_NOT_ALLOWED() {
-//   return new Response(JSON.stringify({ message: "Method Not Allowed" }), {
-//     status: 405,
-//   });
-// }
